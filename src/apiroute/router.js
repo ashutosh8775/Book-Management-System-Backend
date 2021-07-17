0f90ec83468e1e18ceb2180afb8d2ca0062abb52
phani2206 <ankemphanikumar252@live.com>
2021-07-17 21:30:31 +0530

    book router changes

--- src/apiroute/router.js
+++ src/apiroute/router.js
@@ -86,6 +86,31 @@ Router.post("/addReview", (request, response) => {
     });  
 });
 
-
+Router.get("/books",(request,response)=>{
+    mysqlConnection.query(`Select * from Books `,(err,data) =>{
+        if(err){
+            response.send(err);
+        } else{
+            let resp = {
+                "issuccess":0,
+                "output":data
+            }
+            response.send(resp);
+        }
+    });
+})
+Router.get("/books/:id",(request,response)=>{
+    mysqlConnection.query(`Select * from books where id = '${request.params.id}'`,(err,data) =>{
+        if(err){
+            response.send(err);
+        } else{
+            let resp = {
+                "issuccess":0,
+                "output":data
+            }
+            response.send(resp);
+        }
+    });
+})
 
 module.exports = Router;
